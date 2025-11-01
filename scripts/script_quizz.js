5b1622b0cfc3a96147c75a40de99943999c91c51
CDasse <clemence.dasse@yahoo.fr>
2025-11-01 15:28:38 +0100

    feat: add files

---
+++ scripts/script_quizz.js
@@ -0,0 +1,19 @@
+function reponseQuestion1 () {
+    const reponse1 = document.getElementById("question1");
+    reponse1.innerHTML = "<strong>Zeus</strong> est le roi des dieux dans la mythologie grecque.";
+}
+
+function reponseQuestion2 () {
+    const reponse2 = document.getElementById("question2");
+    reponse2.innerHTML = "Le symbole principal de Zeus est <strong>la foudre</strong>.";
+}
+
+function reponseQuestion3 () {
+    const reponse3 = document.getElementById("question3");
+    reponse3.innerHTML = "<strong>Le thyrse, la vigne et le raisin</strong> sont associés à Dionysos.";
+}
+
+function reponseQuestion4 () {
+    const reponse4 = document.getElementById("question4");
+    reponse4.innerHTML = "Athéna est la déesse de la sagesse et de <strong>la guerre stratégique</strong>.";
+}
